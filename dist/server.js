10fb2aced86a1373c39085e3114960a30a2002df
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2020-12-20 14:26:03 -0700

    adjusting paths for heroku

--- dist/server.js
+++ dist/server.js
@@ -6,19 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = express_1.default();
-// Serve static files from the React app
-app.use(express_1.default.static(path_1.default.join(__dirname, 'client/build')));
 app.get("/test", (req, res) => {
     console.log("/test");
     res.json({ message: "Hello World" });
 });
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname + '/client/build/index.html'));
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//   });
+// Don't touch the following - Heroku gets very finnicky about it
+// Serve static files from the React app
+app.use(express_1.default.static(path_1.default.join(__dirname, 'client/build')));
+const root = path_1.default.join(__dirname, '..', 'client', 'build');
+app.use(express_1.default.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+    console.log(`server started on port ${port}`);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,31 @@ import path from 'path';
 
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/test", (req, res) => {
   console.log("/test")
   res.json({message: "Hello World"});
 })
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//   });
+
+
+// Don't touch the following - Heroku gets very finnicky about it
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+const root = path.join(__dirname, '..', 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+	console.log(`server started on port ${port}`)
 });
